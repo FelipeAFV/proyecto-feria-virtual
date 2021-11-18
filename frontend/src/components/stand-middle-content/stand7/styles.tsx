3c3558098c602a66db66c5312769716358abe968
FelipeAFV <felipeafigueroav@gmail.com>
2021-11-18 14:20:02 -0300

    correcciones responsive

--- frontend/src/components/stand-middle-content/stand7/styles.tsx
+++ frontend/src/components/stand-middle-content/stand7/styles.tsx
@@ -2,9 +2,14 @@ import { CSSProperties } from "react";
 
 const infoImageStyle: CSSProperties = {
     height: '300px',
-    width: 'auto',
-    position: 'static',
-    top: '150px',
+    width: '100%',
+    // position: 'relative',
+    // top: '150px',
+    // left: '-150px',
+    // marginLeft: '10%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     margin: '40px auto'
 }
 

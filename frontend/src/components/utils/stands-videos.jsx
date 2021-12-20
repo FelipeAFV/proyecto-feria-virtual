9c1d7957c193750ec4d5f0c004563879231a7b64
FelipeAFV <felipeafigueroav@gmail.com>
2021-12-20 16:24:39 -0300

    responsive changes

--- frontend/src/components/utils/stands-videos.jsx
+++ frontend/src/components/utils/stands-videos.jsx
@@ -2,10 +2,10 @@ import { ReactComponentElement, ReactElement } from "react";
 
 
 const standVideos = [
-    { mainVideo: <iframe width="560" height="315" src="https://www.youtube.com/embed/P2gzKd3KcV0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
-    { mainVideo: <iframe width="560" height="315" src="https://www.youtube.com/embed/zH77XgU7wmk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
-    { mainVideo:<iframe width="560" height="315" src="https://www.youtube.com/embed/i15FCUkvwu8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
-    { mainVideo: <iframe width="560" height="315" src="https://www.youtube.com/embed/33fY56eAdCg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
+    { mainVideo: <iframe className="presentation-video" width="560" height="315" src="https://www.youtube.com/embed/P2gzKd3KcV0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
+    { mainVideo: <iframe className="presentation-video" width="560" height="315" src="https://www.youtube.com/embed/zH77XgU7wmk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
+    { mainVideo:<iframe className="presentation-video" width="560" height="315" src="https://www.youtube.com/embed/i15FCUkvwu8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
+    { mainVideo: <iframe className="presentation-video" width="560" height="315" src="https://www.youtube.com/embed/33fY56eAdCg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>},
     { mainVideo: <div></div>},
     { mainVideo: <div></div>},
     { mainVideo: <div></div>},
